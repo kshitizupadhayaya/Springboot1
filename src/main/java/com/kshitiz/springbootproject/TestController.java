edd8baccf1c28d6b84ef699e04339a7c53544003
kshitiz <kxupadhayaya@gmail.com>
2024-04-20 11:38:36 +0545

    first commit

---
+++ src/main/java/com/kshitiz/springbootproject/TestController.java
@@ -0,0 +1,18 @@
+package com.kshitiz.springbootproject;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+public class TestController
+{
+    @GetMapping("/test")
+    public String firstHandler()
+    {
+        return "just  ||";
+    }
+
+}
